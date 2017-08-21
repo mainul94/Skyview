6be0bd002a35554d330d6997643c61d70440570e
Mainul Islam <mainulkhan94@gmail.com>
2017-08-22 00:45:22 +0600

    For the check auto push

--- database/migrations/2017_08_20_163829_create_categories_table.php
+++ database/migrations/2017_08_20_163829_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $this->schemaCreateStartWith($table);
+            $this->schemaCreateEndWith($table);
         });
     }
 
