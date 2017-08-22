f3c6bb69c2a99c4282eeec41fad1a0b9b961c706
Mainul Islam <mainulkhan94@gmail.com>
2017-08-23 00:07:07 +0600

    Casting value and set value for setting

--- app/Model/Admin/Setting.php
+++ app/Model/Admin/Setting.php
@@ -6,11 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    protected $fillable = ['property', 'cast_type', 'opts'];
+
+    protected $primaryKey = 'property';
+
+
+    protected $casts = ["opts"=>"string"];
+
+
     protected function getCastType($key) {
         if ($key == 'opts' && !empty($this->cast_type)) {
             return $this->cast_type;
         } else {
             return parent::getCastType($key);
+        }
+    }
+
+
+    public function setOptsAttribute($value)
+    {
+        if (in_array(gettype($value), ['object', 'array'])) {
+            $this->attributes['opts'] = json_encode($value);
+        } else if (gettype($value !="string")){
+            $this->attributes['opts'] = (string) $value;
         }
     }
 }
