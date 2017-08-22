4afa580d7b12ba1ee9a4d92f80ce55d99207502a
Mainul Islam <mainulkhan94@gmail.com>
2017-08-22 19:25:05 +0600

    Wroted categories table schema

--- database/migrations/2017_08_20_163829_create_categories_table.php
+++ database/migrations/2017_08_20_163829_create_categories_table.php
@@ -16,6 +16,15 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $this->schemaCreateStartWith($table);
+            $table->longText('content')->nullable();
+            $table->text('summery')->nullable();
+            $table->string('meta_key')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->unsignedInteger('template');
+            $table->unsignedInteger('thumbnail')->nullable();
+            $table->unsignedInteger('slide_id')->nullable();
+            $table->enum('status', ['Published'=>'Published', 'Unpublished'=>'Unpublished'])->default('Published');
+            $table->timestamp('publish_at')->useCurrent();
             $this->schemaCreateEndWith($table);
         });
     }
