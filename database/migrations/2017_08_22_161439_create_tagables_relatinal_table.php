8edd0732bbc2ac210da3d1655b67b4352ab53572
Mainul Islam <mainulkhan94@gmail.com>
2017-08-22 22:17:30 +0600

    Created Tagable relational table.

---
+++ database/migrations/2017_08_22_161439_create_tagables_relatinal_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagablesRelatinalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->unsignedInteger('tag_id');
+            $table->unsignedInteger('taggable_id');
+            $table->string('taggable_type');
+            $table->primary(['tag_id', 'taggable_id','taggable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
